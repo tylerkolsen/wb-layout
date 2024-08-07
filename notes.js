a238675811524b50ebfc364a326b135e4f1b4ff7
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-07 14:31:30 -0600

    added CSS and notes files

---
+++ notes.js
@@ -0,0 +1,37 @@
+// font for header
+// property {
+//     color:rgb(255, 255, 255);
+//     font-family:D-DIN-Bold, Arial, Verdana, sans-serif;
+//     font-size:14px;
+//     font-stretch:100%;
+//     font-style:normal;
+//     font-variant-caps:normal;
+//     font-variant-east-asian:normal;
+//     font-variant-ligatures:normal;
+//     font-variant-numeric:normal;
+//     font-weight:500;
+//     cursor:pointer;
+//     display:block;
+//     line-height:1.4;
+//     position:relative;
+//     text-decoration-color:rgb(255, 255, 255);
+//     text-decoration-line:none;
+//     text-decoration-style:solid;
+//     text-transform:uppercase;
+//     text-decoration-thickness:auto;
+//     ">Falcon 9<span style="background-position:initial;
+//     background-repeat:initial;
+//     -webkit-background-clip:border-box;
+//     background-attachment:scroll;
+//     background-clip:border-box;
+//     background-color:rgb(255, 255, 255);
+//     background-image:none;background-origin:padding-box;
+//     background-size:auto;
+//     bottom:0px;
+//     height:1px;
+//     left:0px;
+//     position:absolute;
+//     transform:matrix(0, 0, 0, 1, 0, 0);
+//     transform-origin:57.6719px 0.5px;
+//     width:100%;
+// }
